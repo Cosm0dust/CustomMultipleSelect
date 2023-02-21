06b9524c9bded10bce262979ebf2325c1a5b6aca
Cosm0dust <vanyakotelevskiy@gmail.com>
2023-02-21 21:30:00 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import { Select, SelectOption } from "./Select"
+
+const options = [
+    { label: "First", value: 1 },
+    { label: "Second", value: 2 },
+    { label: "Third", value: 3 },
+    { label: "Fourth", value: 4 },
+    { label: "Fifth", value: 5 },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value1, setValue1] = useState<SelectOption[]>([options[0]])
+    const [value2, setValue2] = useState<SelectOption | undefined>(options[0])
+
+    return (
+        <>
+            <Select
+                multiple
+                options={options}
+                value={value1}
+                onChange={o => setValue1(o)}
+            />
+            <br />
+            <Select options={options} value={value2} onChange={o => setValue2(o)} />
+        </>
+    )
 }
 
-export default App;
+export default App
